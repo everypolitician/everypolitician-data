e753ca8414e467e747850e33fe9cda4a504700ea
Tony Bowden <tony@tmtm.com>
2018-10-10 19:31:31 +0100

    Look up file change times via Octokit if no local history
    The everypolitician-bot works with a shallow clone of the repo, with no
    history, so if it asks `git` for the last change time of a file, it will
    be seconds ago. So make it so that we look up the date via the Github
    API instead, unless EP_FULL_GIT is set in the environment.

--- rake_build/generate_stats.rb
+++ rake_build/generate_stats.rb
@@ -5,15 +5,34 @@
 #-----------------------------------------------------------------------
 
 require 'date' # To give us DateTime.now
+require 'octokit'
 
 STATSFILE = Pathname.new('unstable/stats.json')
 
 namespace :stats do
+  def local_git_lastmod(file)
+    Date.parse `git log -1 --format="%ai" -- #{file}`.split.first
+  end
+
+  def octokit
+    @octokit ||= Octokit::Client.new
+  end
+
+  def datapath
+    @datapath ||= Pathname.pwd.relative_path_from(PROJECT.realpath)
+  end
+
+  def github_lastmod(file)
+    lc = octokit.commits('everypolitician/everypolitician-data', path: datapath + file).first
+    lc.commit.author.date.to_date
+  end
+
   def lastmod(source)
     path = Pathname('sources') + source[:file]
-    lm = `git log -1 --format="%ai" -- #{path}`.split.first
+    lm = ENV['EP_FULL_GIT'] ? local_git_lastmod(path) : github_lastmod(path)
+
     if source.key? :create
-      elapsed = (DateTime.now - Date.parse(lm)).to_i
+      elapsed = (DateTime.now - lm).to_i
       warn "  ☢  #{source[:file]} has not been updated for #{elapsed} days" if elapsed > 90
     end
     lm
