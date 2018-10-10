69a17369d5d80697296ed18bc1bcc6d15bb02e54
Tony Bowden <tony@tmtm.com>
2018-10-10 15:38:49 +0100

    add information on when sources were last built
    In the statsfile generated for each legislature, include information on
    when each source was last built.

--- rake_build/generate_stats.rb
+++ rake_build/generate_stats.rb
@@ -7,8 +7,21 @@
 STATSFILE = Pathname.new('unstable/stats.json')
 
 namespace :stats do
+  def lastmod(file)
+    path = Pathname('sources') + file
+    `git log -1 --format="%ai" -- #{path}`.split.first
+  end
+
   task :regenerate do
     stats = StatsFile.new(popolo: ep_popolo, position_file: POSITION_CSV).stats
+    stats[:sources] = json_load(@INSTRUCTIONS_FILE)[:sources].map do |src|
+      {
+        file:    src[:file],
+        type:    src[:type],
+        scraper: src.dig(:create, :scraper),
+        lastmod: lastmod(src[:file]),
+      }
+    end
     STATSFILE.dirname.mkpath
     STATSFILE.write(JSON.pretty_generate(stats))
   end
